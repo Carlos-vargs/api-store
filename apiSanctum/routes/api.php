c5f173671feaa40cf4990b92966f0fff5a5f1655
Carlos-vargs <cvargaslopez769@gmail.com>
2021-08-26 10:14:49 -0600

    add some changes

--- apiSanctum/routes/api.php
+++ apiSanctum/routes/api.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::apiResource('v1/products', App\Http\Controllers\Api\V1\ProductController::class);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
--- fronted-store/src/App.js
+++ fronted-store/src/App.js
@@ -3,6 +3,6 @@ import React  from 'react';
 
 export function App() {
   return (
-    d
+    <div>Example</div>
   )
 }
